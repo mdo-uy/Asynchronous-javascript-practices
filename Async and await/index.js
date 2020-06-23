850cabf31dd576e2a86b1330fa1c43ef2c76b352
Mauricio De Oliveira <mauricio.d.oliveira1@gmail.com>
2020-06-23 20:05:52 -0300

    Async and await - add try and catch

--- Async and await/index.js
+++ Async and await/index.js
@@ -1,10 +1,15 @@
 console.log('Before');
 
 async function displayCommits() { 
-    const user = await getUser(1);
-    const repos = await getRepositories(user.gitHubUsername);    
-    const commits = await getCommits(repos[0]);
-    console.log(commits);
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);    
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message);
+    }
 }
 displayCommits();
 
